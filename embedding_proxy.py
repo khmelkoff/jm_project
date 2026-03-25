bd4a0e481f891e8011174ac8a66c31a1c0810c97
khmelkoff <khmelkoff@gmail.com>
2026-03-25 14:38:20 +0300

    Fixed model arg

--- embedding_proxy.py
+++ embedding_proxy.py
@@ -1,25 +1,33 @@
-import json
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 from typing import List, Union
-
+import argparse
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+parser = argparse.ArgumentParser(description="Run the embedding proxy server")
+parser.add_argument("--port", type=int, default=11435, help="Port to run the server on")
+parser.add_argument("--model", type=str, default="deepvk/user-bge-m3", help="Embedding model name")
+parser.add_argument("--reload", action="store_true", help="Enable auto-reload for development")
+parser.add_argument("--verbose", action="store_true", help="Enable diagnostic printing")
+args = parser.parse_args()
 
 app = FastAPI()
 
 # hf_embedding = HuggingFaceEmbeddings(model_name="BAAI/bge-m3", model_kwargs={"device": 'cuda'})
-hf_embedding = HuggingFaceEmbeddings(model_name="deepvk/user-bge-m3", model_kwargs={"device": 'cuda'})
+hf_embedding = HuggingFaceEmbeddings(model_name=args.model, model_kwargs={"device": 'cuda'})
+
 
 class EmbeddingRequest(BaseModel):
     input: Union[str, List[str]]
     model: str
 
+
 class EmbeddingResponse(BaseModel):
     object: str
     data: List[dict]
     model: str
-    # usage: dict
+
 
 @app.post("/embeddings")
 async def create_embedding(request: EmbeddingRequest):
@@ -27,14 +35,12 @@ async def create_embedding(request: EmbeddingRequest):
         request.input = [request.input]
 
     for text in request.input:
-        print('INPUT TEXT:')
-        print(text[:100])
-        print()
+        if args.verbose:
+            print(f'INPUT TEXT:\n{text[:100]}...\n')
 
     ollama_requests = [{"model": request.model, "prompt": text} for text in request.input]
 
     embeddings = []
-
 
     for i, ollama_request in enumerate(ollama_requests):
 
@@ -43,9 +49,8 @@ async def create_embedding(request: EmbeddingRequest):
 
         result = {"embedding": vector}
 
-        print(f"OUTPUT RESULT {i}")
-        print(result["embedding"][:10])
-        print()
+        if args.verbose:
+            print(f'OUTPUT RESULT {i}\n{result["embedding"][:10]}...\n')
 
         embeddings.append({
             "object": "embedding",
@@ -60,10 +65,5 @@ async def create_embedding(request: EmbeddingRequest):
     )
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Run the embedding proxy server")
-    parser.add_argument("--port", type=int, default=11435, help="Port to run the server on")
-    parser.add_argument("--reload", action="store_true", help="Enable auto-reload for development")
-    args = parser.parse_args()
 
     uvicorn.run("embedding_proxy:app", host="0.0.0.0", port=args.port, reload=args.reload)
